0ed0720b98ba954d84a1a0fba94a34b03e3f44b9
Vomitadyo <vomitadyo@gmail.com>
2026-08-10 18:27:23 -0600

    completed project

--- Build/Code/GIS.R
+++ Build/Code/GIS.R
@@ -6,12 +6,17 @@ rm(list = ls())
 library(sf)
 library(leaflet)
 library(dplyr)
+library(ggplot2)
 
 # Import our processed data in Cache
-Data_A <- read.csv('Build/Data/Data_A.csv')
-Data_B <- read.csv('Build/Data/Data_B.csv') 
 
-My_df <- merge(Data_A, Data_B) 
+xxxxxxxxxx
+
+# if you cant use the df created earlier,  run the codes below to create it
+#Data_A <- read.csv('Build/Data/Data_A.csv')
+#Data_B <- read.csv('Build/Data/Data_B.csv') 
+#My_df <- merge(Data_A, Data_B) 
+
 
 # Convert to sf file
 data_sf <- My_df %>%
@@ -32,11 +37,9 @@ ggplot(data_sf) +
 
 # Read Other Maps
 # CO Counties in study area
-Counties <- read_sf("Build/Data/counties_shapefiles/USA_Counties.shp") %>%
-  filter(STATE_NAME == 'Colorado')%>%
-  filter(NAME %in% c('Cheyenne', 'Kit Carson', 'Lincoln', 'Logan', 'Phillips', 'Sedgwick', 'Washington', 'Yuma')) %>%
-  st_transform(4326)
+Counties <- read_sf("Build/Data/Counties/Counties.shp")
 
+# find central points to use for labeling
 County_Labels <- st_point_on_surface(Counties)
 
 
